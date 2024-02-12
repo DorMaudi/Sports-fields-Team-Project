3a2e362f12bbca8de5a665038431cb46e95a17bf
Dor Maudi <157501429+DorMaudi@users.noreply.github.com>
2024-02-12 12:16:36 +0200

    refactor the code to windows
    factor the code to functions that works with windows

---
+++ cmake-build-debug/CMakeFiles/Sports_fields_Team_Project.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Sports_fields_Team_Project.
+# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 int main() {
 
     // make a new db.
-    db dataBase("/Users/maudi/Documents/Dev/Sports-fields-Team-Project/dataBase/users",
-                "/Users/maudi/Documents/Dev/Sports-fields-Team-Project/dataBase/fields");
+    db dataBase(R"(C:\Users\dorma\Documents\gitProjects\Sports-fields-Team-Project\dataBase\users)",
+                R"(C:\Users\dorma\Documents\gitProjects\Sports-fields-Team-Project\dataBase\fields)");
 
 
     // load every user and field from the db.
